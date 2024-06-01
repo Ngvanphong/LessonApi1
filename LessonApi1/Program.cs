5f62559e9c84277910ebdb24ad654a6b05dccd7d
Nguyen van Phong <ngvanphong2012@gmail.com>
2024-06-01 16:13:38 +0700

    Add project files.

---
+++ LessonApi1/Program.cs
@@ -0,0 +1,85 @@
+﻿
+// in ra mang hinh;
+//
+//
+
+
+/*
+ * this is comment 
+ * this ti htis 
+ * 
+ * 
+ */
+
+int a = 2;
+int b = 3;
+int totalInt = a + b; // cu phap lac da
+
+double c = 0;
+
+bool isTrue= true;  // tra ve true or false;
+
+isTrue = 10 == 11;
+
+
+// Type 
+
+short numberShort=-1;
+
+ushort numberUShort=1;
+
+int numerInteger=-10;
+
+uint numerUInteger=10;
+
+long numerLong = 10;
+
+ulong numerULong= 10;
+
+float numerFloat = 10.1f;
+
+double numberDouble = 10;
+
+char textChar = 'a';
+
+string textMultiString = "Monday \n Tuesday";
+
+string textMultiString2 = @"Monday \n Tuesday";
+
+decimal numberDecimal = -10000000;
+
+
+double number1 = 7;
+double number2 = 5;
+
+double totalNumner= number1 / number2;
+
+bool isCompare= !(number1 <= number2);
+
+bool isCompare2 = number1 != number2;
+
+
+bool isCompare3 = (number1 != number2) && (number1 != 10);
+
+bool isCompre4 = !(number1 == number2) || (number1 != 10 && number1==10);
+
+
+
+
+number1++;
+number1--;
+number1 += 10;
+number1 -= 10;
+
+double resultNumber= (isCompre4==true && 10==12)? number1 : number2;
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine(isCompare);
